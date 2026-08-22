4819e9ed8776d41f27111da996624006cf76c833
Solo Hacker <165564128+SoloHackerEmpress@users.noreply.github.com>
2026-08-22 18:21:33 +0530

    Update runner.py

--- runner.py
+++ runner.py
@@ -221,11 +221,11 @@ async def main():
       'quiet': True,
       'nocheckcertificate': True,
       'geo_bypass': True,
-      'proxy': 'http://190.61.88.147:8080',
+      'socket_timeout': 30,
       'http_headers': {
           'User-Agent': (
               'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36'
-              ' (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+              ' (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36'
           ),
           'Accept': (
               'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8'
